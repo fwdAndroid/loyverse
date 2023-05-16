f8fac227a8e4fa8e0028f6ab90e6cb3ed3d5783b
fwdAndroid <fwdkaleem@gmail.com>
2023-05-16 15:44:06 +0500

    Employees Added

---
+++ lib/dashbaord/itemslist/add_discounts.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:loyverse/dashbaord/itemslist/categorylist.dart';
+import 'package:loyverse/dashbaord/itemslist/discountlist.dart';
+import 'package:loyverse/dashbaord/itemslist/itemslist.dart';
+import 'package:loyverse/widgets/my_drawer.dart';
+
+enum SingingCharacter { sold, volume }
+
+class AddDiscounts extends StatefulWidget {
+  const AddDiscounts({super.key});
+
+  @override
+  State<AddDiscounts> createState() => _AddDiscountsState();
+}
+
+class _AddDiscountsState extends State<AddDiscounts> {
+  SingingCharacter? _character = SingingCharacter.sold;
+  List<String> displayList = <String>[
+    'Category 1',
+    "Category 2",
+    "Category 3",
+    "Category 4",
+  ];
+  String drop = "Category 1";
+  bool _lights = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Create Discount",
+          style: TextStyle(
+              color: Color(0xff214325),
+              fontSize: 20,
+              fontWeight: FontWeight.w800),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      drawer: MyDrawer(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              margin: EdgeInsets.only(top: 15, left: 20),
+              child: Text(
+                "Name",
+                style: TextStyle(fontSize: 17),
+              )),
+          Container(
+              margin: EdgeInsets.only(top: 5, left: 20, right: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+
+                      borderSide: BorderSide(
+                          width: 1, color: Color(0xffF5F6FA)), //<-- SEE HERE
+                    ),
+                    border: InputBorder.none,
+                    hintText: "Fawad Kaleem",
+                    fillColor: Color(0xffF5F6FA),
+                    filled: true),
+              )),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+              margin: EdgeInsets.only(top: 5, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: ListTile(
+                      title: const Text(
+                        'Each',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      leading: Radio(
+                        value: SingingCharacter.sold,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListTile(
+                      title: const Text(
+                        'Weight/Volume',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      leading: Radio(
+                        value: SingingCharacter.volume,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+          Container(
+              margin: EdgeInsets.only(top: 15, left: 20),
+              child: Text(
+                "Value",
+                style: TextStyle(fontSize: 17),
+              )),
+          Container(
+              margin: EdgeInsets.only(top: 5, left: 20, right: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+
+                      borderSide: BorderSide(
+                          width: 1, color: Color(0xffF5F6FA)), //<-- SEE HERE
+                    ),
+                    border: InputBorder.none,
+                    hintText: "Fawad Kaleem",
+                    fillColor: Color(0xffF5F6FA),
+                    filled: true),
+              )),
+          SizedBox(
+            height: 10,
+          ),
+          CheckboxListTile(
+            title: const Text('The item is available of sale'),
+            value: timeDilation != 1.0,
+            onChanged: (bool? value) {
+              setState(() {
+                timeDilation = value! ? 10.0 : 1.0;
+              });
+            },
+            controlAffinity: ListTileControlAffinity.leading,
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (builder) => DiscountList()));
+                  },
+                  child: Text("Save"),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (builder) => DiscountList()));
+                  },
+                  child: Text("Cancel"),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/dashbaord/itemslist/discountlist.dart
+++ lib/dashbaord/itemslist/discountlist.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:loyverse/dashbaord/itemslist/add_category.dart';
+import 'package:loyverse/dashbaord/itemslist/add_discounts.dart';
+import 'package:loyverse/dashbaord/itemslist/add_item.dart';
+import 'package:loyverse/dashbaord/itemslist/add_modifier.dart';
+import 'package:loyverse/widgets/my_drawer.dart';
 
 class DiscountList extends StatefulWidget {
   const DiscountList({super.key});
@@ -11,6 +17,110 @@ class DiscountList extends StatefulWidget {
 class _DiscountListState extends State<DiscountList> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Discounts",
+          style: TextStyle(
+              color: Color(0xff214325),
+              fontSize: 20,
+              fontWeight: FontWeight.w800),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xff394867),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (builder) => AddDiscounts()));
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      drawer: MyDrawer(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              margin: EdgeInsets.only(left: 20, right: 20, top: 40),
+              child: Text("Discounts")),
+          Container(
+            height: MediaQuery.of(context).size.height / 2,
+            margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+            child: ListView.builder(
+              itemCount: 2,
+              itemBuilder: (context, i) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xff9BA4B5),
+                        borderRadius: BorderRadius.circular(12)),
+                    child: CheckboxListTile(
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Value',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          const Text(
+                            '12%',
+                            style: TextStyle(color: Colors.black, fontSize: 10),
+                          ),
+                        ],
+                      ),
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Name',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          const Text(
+                            'asdasd',
+                            style: TextStyle(color: Colors.black, fontSize: 10),
+                          ),
+                        ],
+                      ),
+                      value: timeDilation != 1.0,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          timeDilation = value! ? 10.0 : 1.0;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity.leading,
+                      secondary: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Restricted Access',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 40),
+                            child: const Text(
+                              'NO',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 10),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
